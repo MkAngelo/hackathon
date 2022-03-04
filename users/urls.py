601af780c24bccbd9571f5e54cf68649684949b0
MkAngelo <mikequi000@gmail.com>
2022-03-04 11:00:06 -0600

    bootstrap

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,11 @@
 """Users URLs"""
 
+from unicodedata import name
 from django.urls import path
 
 from users import views
 
 urlpatterns =[
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('denuncia/', views.denuncia, name='denuncia'),
 ]
--- users/views.py
+++ users/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    "Home page"
+    """Home page"""
     return render(request, 'users/home.html')
+
+def denuncia(request):
+    """Denuncia page"""
+    return render(request, 'users/denuncia.html')
