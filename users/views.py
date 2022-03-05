eb21ced89bdc9664f564fac29c38cfe43c8343b8
MkAngelo <mikequi000@gmail.com>
2022-03-04 23:25:08 -0600

    HOTFIX  EDAD

--- users/views.py
+++ users/views.py
@@ -11,11 +11,12 @@ def denuncia(request):
     """Denuncia page"""
     if request.method == 'POST':
         fecha = request.POST['fecha']
+        descri = request.POST['descripcion']
         nombre_den = request.POST['nombre_denunciado']
         ocupacion_den = request.POST['ocupacion_denunciado']
         lugar = request.POST['lugar']
         
-        if fecha == "" or nombre_den == "" or ocupacion_den == "" or lugar == "":
+        if fecha == "" or descri == "" or ocupacion_den == "" or lugar == "":
             return render(request, 'users/denuncia.html', {'error': 'Necesitas llenar los campos que tienen un (*)'})
         report = Report.objects.create(
             datetime = fecha,
@@ -24,14 +25,21 @@ def denuncia(request):
             ocupacion_d = ocupacion_den,
             name = request.POST['nombre_victima'],
             sexo = request.POST['sexo'],
-            edad = request.POST['edad'],
             ocupacion = request.POST['ocupacion'],
             escolaridad_d = request.POST['escolaridad_denunciado'],
-            descripcion = request.POST['descripcion'],
+            descripcion = descri,
             imagen = request.POST['foto'],
             video = request.POST['video'],
-            email = request.POST['email']
+            email = request.POST['email'],
         )
+
+        if(request.POST['edad'] == ""):
+            report.edad = 0;
+        else:
+            report.edad = request.POST['edad']
+
+        report.save()
+
         return redirect('users:home')
 
     return render(request, 'users/denuncia.html')
